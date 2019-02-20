1e2f4714ba8fa32e8a353f9d4ef400df9f13e3f9
edwin ramos <edwinramos269@gmail.com>
2019-02-20 20:57:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
-# your code goes here
+def begins_with_r(array)
+  array.all?{|string|string.include?'r'}
+end
+
+def contain_a(array)
+  array2=[]
+  array.each do |item|
+    if item.include?'a'
+      array2.push(item)
+    else
+  end
+end
+  array2
+end 
+
+def first_wa(array)
+ array.find{|item| item.to_s.include?"wa"}
+end
+
+def remove_non_strings(array)
+ array.keep_if{|item| item.is_a? String}
+ end
+  
+ def count_elements(array)
+   i=0
+   array2=[]
+   array.each do{ |item|
+   if item=array.[i]
+     
+    
+   else
+     i=i+1
+   end
+     
