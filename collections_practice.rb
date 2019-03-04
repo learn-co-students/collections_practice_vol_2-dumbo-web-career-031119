77425c27b1fd94741137ccb73df154b108754d64
edwin ramos <edwinramos269@gmail.com>
2019-03-04 18:55:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def begins_with_r(array)
   array.all?{|string|string.include?'r'}
 end
@@ -22,13 +24,44 @@ def remove_non_strings(array)
  end
   
  def count_elements(array)
-   i=0
-   array2=[]
-   array.each do{ |item|
-   if item=array.[i]
-     
-    
-   else
-     i=i+1
+  array.each do |key|
+    key[:count]= 0
+    array.each do |hash|
+    if hash[:name]== key[:name]
+      key[:count] +=1
+    end 
+  end 
+end.uniq
+ end
+ 
+ def merge_data(keys,data)
+   hash={}
+     data[0].map do |key,value|
+       keys.map do |name_hash|
+         if name_hash[:first_name]== key
+           hash=value.merge(name_hash)
+   end 
+ end
+ hash
+ end
+ end
+ 
+ def find_cool(cool)
+   cool.each do |data|
+     if data[:temperature]=="cool"
+       return [data]
+ end 
+ end 
+ end
+ 
+ def organize_schools(schools)
+   answer={}
+   schools.each do |school,location|
+     if answer.key?(location[:location])
+       answer[location[:location]] << school
+     else 
+       answer[location[:location]] = [school]
    end
-     
+end 
+answer
+end
